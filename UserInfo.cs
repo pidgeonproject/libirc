9e670638e0d2e2995581ab4424a37b98d32ae3aa
Petr Bena <benapetr@gmail.com>
2014-05-24 14:22:22 +0200

    removed tabs

--- UserInfo.cs
+++ UserInfo.cs
@@ -17,46 +17,46 @@ using System;
 
 namespace libirc
 {
-	/// <summary>
-	/// Low level object with basic IRC user info
-	/// </summary>
-	public class UserInfo
-	{
-		public string Nick;
-		public string Ident;
-		public string Host;
+    /// <summary>
+    /// Low level object with basic IRC user info
+    /// </summary>
+    public class UserInfo
+    {
+        public string Nick;
+        public string Ident;
+        public string Host;
 
-		public UserInfo()
-		{
-			this.Nick = null;
-			this.Ident = null;
-			this.Host = null;
-		}
+        public UserInfo()
+        {
+            this.Nick = null;
+            this.Ident = null;
+            this.Host = null;
+        }
 
-		public UserInfo(string nick, string ident, string host)
-		{
-			this.Nick = nick;
-			this.Ident = ident;
-			this.Host = host;
-		}
+        public UserInfo(string nick, string ident, string host)
+        {
+            this.Nick = nick;
+            this.Ident = ident;
+            this.Host = host;
+        }
 
-		public UserInfo(string source)
-		{
-			if (source.Contains("!"))
-			{
-				this.Nick = source.Substring(0, source.LastIndexOf("!", StringComparison.Ordinal));
-				this.Ident = source.Substring(source.LastIndexOf("!") + 1);
-				if (this.Ident.Contains("@"))
-				{
-					this.Host = this.Ident.Substring(this.Ident.LastIndexOf("@") + 1);
-					this.Ident = this.Ident.Substring(0, this.Ident.LastIndexOf("@"));
-				}
-			}
-			else
-			{
-				this.Nick = source;
-			}
-		}
-	}
+        public UserInfo(string source)
+        {
+            if (source.Contains("!"))
+            {
+                this.Nick = source.Substring(0, source.LastIndexOf("!", StringComparison.Ordinal));
+                this.Ident = source.Substring(source.LastIndexOf("!") + 1);
+                if (this.Ident.Contains("@"))
+                {
+                    this.Host = this.Ident.Substring(this.Ident.LastIndexOf("@") + 1);
+                    this.Ident = this.Ident.Substring(0, this.Ident.LastIndexOf("@"));
+                }
+            }
+            else
+            {
+                this.Nick = source;
+            }
+        }
+    }
 }
 
